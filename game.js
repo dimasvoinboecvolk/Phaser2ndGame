09e3102df47aa03b294179186e7c4efc30a4519d
dimasvoinboecvolk <158043890+dimasvoinboecvolk@users.noreply.github.com>
2024-03-18 14:37:06 +0200

    додав lives

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 200 },
             debug: false
         }
     },
@@ -25,6 +25,8 @@ var gameOver = false;
 var stars;
 var bombs;
 var worldWidth = 9600;
+var lives = 3;
+var livesText;
 
 var game = new Phaser.Game(config);
 
@@ -38,6 +40,7 @@ function preload() {
     this.load.image('asset1', 'assets/asset1.png');
     this.load.image('asset2', 'assets/asset2.png');
     this.load.image('asset3', 'assets/asset3.png');
+    this.load.image('healthPack', 'assets\heart-outline-filled.png');
 
     //повітряні платформи
     this.load.image('skyGroundStart', 'assets/14.png');
@@ -65,6 +68,11 @@ function create() {
     }
 
 
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
+
+
+    livesText = this.add.text(16, 50, 'Lives: ' + lives, { fontSize: '32px', fill: '#fff' });
+
     //створення гравця
     player = this.physics.add.sprite(100, 450, 'dude');
 
@@ -84,30 +92,30 @@ function create() {
     rock = this.physics.add.staticGroup();
 
     for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(300, 500)) {
-    
+
         rock.create(x, 1080 - 128, 'asset1')
-        .setOrigin(0, 0.8)
-        .setScale(Phaser.Math.FloatBetween(0.3, 0.6))
-        .setDepth(Phaser.Math.Between(1,10));
+            .setOrigin(0, 0.8)
+            .setScale(Phaser.Math.FloatBetween(0.3, 0.6))
+            .setDepth(Phaser.Math.Between(1, 10));
 
     }
 
     for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(100, 200)) {
-    
+
         rock.create(x, 1080 - 128, 'asset2')
-        .setOrigin(0, 0.8)
-        .setScale(Phaser.Math.FloatBetween(0.3, 0.6))
-        .setDepth(Phaser.Math.Between(1,10));
+            .setOrigin(0, 0.8)
+            .setScale(Phaser.Math.FloatBetween(0.3, 0.6))
+            .setDepth(Phaser.Math.Between(1, 10));
 
     }
 
-    
+
     for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(400, 700)) {
-    
+
         rock.create(x, 1080 - 128, 'asset3')
-        .setOrigin(0, 0.8)
-        .setScale(Phaser.Math.FloatBetween(0.3, 0.6))
-        .setDepth(Phaser.Math.Between(1,10));
+            .setOrigin(0, 0.8)
+            .setScale(Phaser.Math.FloatBetween(0.3, 0.6))
+            .setDepth(Phaser.Math.Between(1, 10));
 
     }
 
@@ -117,22 +125,22 @@ function create() {
         var y = Phaser.Math.FloatBetween(300, 700)
         // ліво
         platforms.create(x, y, 'skyGroundStart')
-        .setOrigin(0, 0)
-        .refreshBody();
- // центр
-        platforms.create(x + 128, y , 'skyGround')
-        .setOrigin(0, 0)
-        .refreshBody();
-// право
-        platforms.create(x + 128*2, y, 'skyGroundEnd')
-        .setOrigin(0, 0)
-        .refreshBody();
+            .setOrigin(0, 0)
+            .refreshBody();
+        // центр
+        platforms.create(x + 128, y, 'skyGround')
+            .setOrigin(0, 0)
+            .refreshBody();
+        // право
+        platforms.create(x + 128 * 2, y, 'skyGroundEnd')
+            .setOrigin(0, 0)
+            .refreshBody();
 
-       // Bigplatforms.create(x, 1080 - 50, 'BigP').setOrigin(0, 0).refreshBody();
+        // Bigplatforms.create(x, 1080 - 50, 'BigP').setOrigin(0, 0).refreshBody();
     }
 
-  
-   
+
+
     this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
@@ -159,7 +167,7 @@ function create() {
 
     stars = this.physics.add.group({
         key: 'star',
-        repeat: 25,
+        repeat: 60,
         setXY: { x: 12, y: 0, stepX: 70 }
     });
 
@@ -172,8 +180,6 @@ function create() {
     this.physics.add.collider(stars, platforms);
 
     this.physics.add.overlap(player, stars, collectStar, null, this)
-
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
     bombs = this.physics.add.group();
 
@@ -229,55 +235,60 @@ function update() {
 
 }
 
+var firstStarCollected = false; // Flag to track if the first star is collected
+
+
 function collectStar(player, star) {
     star.disableBody(true, true);
 
-    score += 1;
-    scoreText.setText('Score: ' + score);
+    var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+    var bomb = bombs.create(x, 36, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
 
     if (stars.countActive(true) === 0) {
         stars.children.iterate(function (child) {
-
             child.enableBody(true, child.x, 16, true, true);
-
         });
-
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-        
-        var bomb = bombs.create(x, 36, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        
-
     }
 }
 
+
 function hitBomb(player, bomb) {
-    this.physics.pause();
+
 
     player.setTint(0xff0000);
 
     player.anims.play('turn');
 
-    window.alert("Ви програли\n"); // вивести на екран як модальне вікно
+    lives--; // Decrement lives
+    livesText.setText('Lives: ' + lives); // Update lives text
 
-    gameOver = true;
+    if (lives === 0) {
+        gameOver = true;
+        window.alert("Ви програли\n"); // Display game over message
 
-    location.reload();
-
-
+        location.reload(); // Reload the game
+    } else {
+        // Reset player position or perform other actions
+    }
 }
 
 function hitEnemy(player, enemy) {
-    this.physics.pause(); // Pause the game
+
     player.setTint(0xff0000); // Tint player red
     player.anims.play('turn'); // Display 'turn' animation for player
-    window.alert("Ви програли\n"); // вивести на екран як модальне вікно
 
-    gameOver = true;
+    lives--; // Decrement lives
+    livesText.setText('Lives: ' + lives); // Update lives text
 
-    location.reload();
+    if (lives === 0) {
+        gameOver = true;
+        window.alert("Ви програли\n"); // Display game over message
 
+        location.reload(); // Reload the game
+    } else {
+        // Reset player position or perform other actions
+    }
 }
